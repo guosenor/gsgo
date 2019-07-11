d80446997a63b1fe3830384c8581427f1b5bcd10
guosen <guosen@baokangys.com>
2019-07-11 18:11:35 +0800

    .

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module gsgo
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/Unknwon/com v0.0.0-20190321035513-0fed4efef755
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-ini/ini v1.44.0
+	github.com/jinzhu/gorm v1.9.10 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190710185942-9d28bd7c0945 // indirect
+	golang.org/x/tools v0.0.0-20190710184609-286818132824 // indirect
+	gopkg.in/ini.v1 v1.44.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"gin-blog/pkg/setting"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "test",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
